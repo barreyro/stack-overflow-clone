1c8850339cb924b42f5940ade797e01124b23588
Nicholas Mascia <nmmascia@gmail.com>
2015-01-15 17:59:17 -0500

    User spec: wrote basic validation tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  it "requires a username" do
-    FactoryGirl.build(:user, username: "").should_not be_valid
+  it "creates a user object when given valid attributes" do
+    user = FactoryGirl.build(:user)
+    expect(user).to be_instance_of(User)
+  end
+
+  it "requires a username to save" do
+    user = FactoryGirl.build(:user, username: "")
+    expect(user.save).to be_falsey
+  end
+
+  it "requires an email to save" do
+    user = FactoryGirl.build(:user, email: "")
+    expect(user.save).to be_falsey
   end
 
 end
