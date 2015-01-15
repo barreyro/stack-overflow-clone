a51601b8f5108a0089137cbc238a8ea7c1027919
Luke Thomas <Lukert33@gmail.com>
2015-01-15 17:48:44 -0500

    Amend bad naming in Answer migration. Migration and tests passing.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-spe# encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,10 +10,17 @@ spe# encoding: UTF-8
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20150115213637) do
+
+ActiveRecord::Schema.define(version: 20150115224259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
