21e172e1b55e74a0b65232daf0df9015a9a4d0cc
Nicholas Mascia <nmmascia@gmail.com>
2015-01-15 18:25:15 -0500

    Created relationships to other tables.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  has_many :answers
+  has_many :questions
+  has_many :votes
 end
