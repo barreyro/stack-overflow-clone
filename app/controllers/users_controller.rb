11f7d960dde58439e3f364ef526c976139320aa6
Nicholas Mascia <nmmascia@gmail.com>
2015-01-15 19:51:12 -0500

    Users: created basic routes for user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save!
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = user.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
