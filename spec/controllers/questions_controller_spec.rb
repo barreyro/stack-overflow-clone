34593364c6c12c2c31ac74f4622af750159bb7d0
Stephy B <stephaniebarreyro@gmail.com>
2015-01-16 12:29:10 -0500

    GET #new spec passes for Questions.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -8,5 +8,11 @@ describe QuestionsController do
       expect(response).to be_success
     end
   end
+  context "GET #new" do
+    it "assigns a questiono variable to Question.new" do
+      get :new
+      expect(assigns(:question)).to be_a_new Question
+    end
+  end
 end
 
