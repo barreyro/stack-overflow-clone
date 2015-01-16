0c923ba4bb71df0c157fdc7817015b6d18151e95
Nicholas Mascia <nmmascia@gmail.com>
2015-01-15 20:48:21 -0500

    User Model: Added some more validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates :password, presence: true, length: { within: 6..20 }
 
   has_many :answers
   has_many :questions
