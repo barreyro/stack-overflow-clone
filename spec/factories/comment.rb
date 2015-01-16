e8f91b6fab2131b4d004ec5d63e3e5e888402e70
Daniel K. Chan & Luke Thomas <pair+dkchan90.lukert33@devbootcamp.com>
2015-01-16 12:35:31 -0500

    Write working comment tests.

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :comment do
     association :user
     body { Faker::Lorem.paragraph }
+
+    after(:build) do |comment|
+      question = create(:question)
+      question.comments << comment
+    end
+
   end
 end
