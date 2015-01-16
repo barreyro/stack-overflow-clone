db0c2c71ff89d5eac241245a361103586513a5fa
Nicholas Mascia <nmmascia@gmail.com>
2015-01-16 17:09:56 -0500

    upvotes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,28 @@ class User < ActiveRecord::Base
   def has_questions?
     true if !self.questions.empty?
   end
+
+  def reputation
+    self.upvote_count - self.downvote_count
+  end
+
+  def upvote_count
+    (get_all_upvotes(self.questions) + get_all_upvotes(self.answers)) + (get_all_upvotes(self.answers) + get_all_upvotes(self.answers))
+  end
+
+  def downvote_count
+    self.votes.where(upvote: false).count
+  end
+
+  def get_all_upvotes data_set
+    data_set.each do |data|
+      data.select { |vote| vote.upvote? }.count
+    end
+  end
+
+  def get_all_downvotes data_set
+    data_set.each do |data|
+      data.select { |vote| vote.downvote? }.count
+    end
+  end
 end
