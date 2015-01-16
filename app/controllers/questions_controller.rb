30910e7b9314550c2117437ecd24d722463a53e4
Stephy B <stephaniebarreyro@gmail.com>
2015-01-16 14:27:28 -0500

    Questions#Index view changed to display each question.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,21 @@ class QuestionsController  < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
 end
