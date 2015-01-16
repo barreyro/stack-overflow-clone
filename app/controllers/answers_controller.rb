1d14d6a842b6baeb56fb2837a3a3bac077b41755
Daniel K. Chan <dkchan90@gmail.com>
2015-01-16 16:23:57 -0500

    add answers controller

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,57 @@
+class AnswersController < ApplicationController
+
+  before_filter :get_question
+
+  def get_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def index
+    @answers = @question.answers
+  end
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  def create
+    answer = @question.answers.new(answer_params)
+    if answer.save
+      redirect_to root_path
+    else
+      flash[:failure] = "Answer failed to save."
+      redirect_to new_question_answer_path(@question)
+    end
+  end
+
+  def show
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def edit
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def update
+    answer = @question.answers.find(params[:id])
+    if answer.update(answer_params)
+      flash[:success] = "Answer updated successfully."
+      redirect_to question_answer_path(@question, answer)
+    else
+      flash[:failure] = "Answer failed to update."
+      redirect_to edit_question_answer_path(@question, answer)
+    end
+  end
+
+  def destroy
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+  end
+
+  private
+
+    def answer_params
+      params.require(:answer).permit(:body, :answer_id, :user_id)
+    end
+
+end
