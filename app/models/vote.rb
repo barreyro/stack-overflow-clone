0c92d6f06217517b16871669145da82212ed5fc1
Luke Thomas <Lukert33@gmail.com>
2015-01-15 21:36:48 -0500

    Working answer specs.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :article, polymorphic: true
-  validates_presence_of :upvote
+  validates :upvote, inclusion: { in: [true, false] }
 end
