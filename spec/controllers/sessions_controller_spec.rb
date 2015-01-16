aa09b07078617d5b5e9ef75e54dbe9da988ed8c7
Nicholas Mascia <nmmascia@gmail.com>
2015-01-16 12:55:45 -0500

    Sess Ctrl: Tests written for some routes.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe SessionsController do
+  before :each do
+    @user = build(:user)
+  end
+
   describe "GET #login" do
     it "renders the login template" do
       get :login
@@ -8,10 +12,25 @@ describe SessionsController do
     end
   end
 
-  # describe "GET #create_session" do
-  #   it "finds the correct user before authenticating" do
-  #     get :create_session
-  #     expect(assigns(:user)).to be_a_new(User)
-  #   end
-  # end
+  describe "GET #create_session" do
+    it "sets the correct session user after authenticating" do
+    end
+  end
+
+  describe "GET #logout" do
+    before :each do
+      @user = create(:user)
+      session[:user_id] = @user.id
+    end
+
+    it "removes the user id from session user_id" do
+      get :logout
+      expect(session[:user_id]).to be nil
+    end
+
+    it "redirects to the root_path" do
+      get :logout
+      expect(response).to redirect_to root_path
+    end
+  end
 end
