78ed7b5361244a10b37a87ef33c6bb73b8dd0177
Nicholas Mascia <nmmascia@gmail.com>
2015-01-15 21:44:42 -0500

    Users Controller Spec: Basic tests written for get route.

--- spec/controllers/users_controller.rb
+++ spec/controllers/users_controller.rb
@@ -4,7 +4,19 @@ describe UsersController do
   describe "GET #new" do
     it "assigns a User to @user" do
       get :new
-      expect(assigns(user)).to be_a_new(User)
+      expect(response).to be_success
+    end
+
+    it "renders :new template" do
+      get :new
+      expect(response).to render_template :new
     end
   end
+
+  # describe "POST #create" do
+  #   it "redirects to root_path if user created" do
+  #     post :create, user: attributes_for(:user)
+  #     expect(response).to redirect_to root_path
+  #   end
+  # end
 end
