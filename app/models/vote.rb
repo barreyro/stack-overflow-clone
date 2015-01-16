4ceaacbd7b02a0b18122fb5ef90af975b225e4d4
Luke Thomas <Lukert33@gmail.com>
2015-01-15 21:43:22 -0500

    Build working vote specs.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,13 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :article, polymorphic: true
   validates :upvote, inclusion: { in: [true, false] }
+
+  def upvote?
+    upvote
+  end
+
+  def downvote?
+    !upvote
+  end
+
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe Vote do
-  let(:upvote){ Vote.new(upvote:true)}
-  let(:downvote){ Vote.new(downvote:true)}
+  let(:upv){ create(:upvote)}
+  let(:downv){ create(:downvote)}
 
   it "is invalid if it is note defined as an up or downvote" do
     non_vote = Vote.new
@@ -11,18 +11,21 @@ describe Vote do
   end
 
   it "is either an upvote or a downvote" do
-    expect(upvote.upvote?).to be true
-    expect(downvote.upvote?).to be false
-    expect(downvote.downvote?).to be true
+    expect(upv.upvote?).to be true
+    expect(downv.upvote?).to be false
+    expect(downv.downvote?).to be true
   end
 
   it "can belong to a Question" do
-    question = Question.create(title:"sample")
-    question << upvote
+    question = create(:question)
+    question.votes << upv
+    expect(question.votes).to include upv
   end
 
   it "can belong to an Answer" do
-
+    answer = create(:answer)
+    answer.votes << downv
+    expect(answer.votes).to include downv
   end
 
 end
