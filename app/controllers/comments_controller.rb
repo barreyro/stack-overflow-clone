bef70dab8d989f0dd5633bf166278c544eb4da22
Luke Thomas <Lukert33@gmail.com>
2015-01-17 13:33:47 -0500

    Organize comment actions into scopes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,6 @@
 class CommentsController < ApplicationController
+  #parent_article scoped
   def index
-  end
-
-  def show
   end
 
   def new
@@ -11,6 +9,13 @@ class CommentsController < ApplicationController
   def create
   end
 
+  #self-scoped
+  def show
+  end
+
   def destroy
+  end
+
+  def update
   end
 end
