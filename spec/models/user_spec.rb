d30477f5b509853b0ae8afeef519c6835ba5759c
Nicholas Mascia <nmmascia@gmail.com>
2015-01-17 20:07:41 -0500

    User Spec: testing to support user reputation methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,4 +46,88 @@ describe User do
       expect(current_user.id).to eq(user.id)
     end
   end
+
+  describe "Methods" do
+    before :each do
+      @user = create(:user)
+      @user_question = @user.questions.create({ title: "heyyy", body: "okayyyy" })
+    end
+
+    describe "#votes_on_all_questions" do
+      before :each do
+        10.times do
+          question = @user.questions.create({ title: "Hi", body: "Well, hello there ;)" })
+          5.times { question.votes.create({ user_id: rand(1..20), upvote: [true, false].sample }) }
+        end
+      end
+
+      it "returns a collection of votes" do
+        expect(@user.votes_on_all_questions.all? { |vote| vote.class.eql? Vote }).to be true
+      end
+
+      it "returns the corrent number of votes on a user's question" do
+        expect(@user.votes_on_all_questions.count).to eq 50
+      end
+    end
+
+    describe "#votes_on_all_answers" do
+      before :each do
+        10.times do
+          answer = @user_question.answers.create({ body: "Hay thur, friendzs!"})
+          5.times { answer.votes.create({ user_id: rand(1..20), upvote: [true, false].sample }) }
+        end
+      end
+
+      it "returns a collection of votes" do
+        expect(@user.votes_on_all_answers.all? { |vote| vote.class.eql? Vote }).to be true
+      end
+
+      it "returns the correct number of votes on an answer nested in a question" do
+        expect(@user.votes_on_all_answers.count).to eq 50
+      end
+    end
+  end
+
+  describe "#upvotes and #downvotes and #reputation" do
+    before :each do
+      @user = create(:user)
+      user_question = @user.questions.create({ title: "heyyy", body: "okayyyy" })
+
+      @rand_num = rand(1..20)
+      @rand_num.times do
+        user_question.votes.create({ user_id: rand(1..20), upvote: true })
+        user_question.votes.create({ user_id: rand(1..20), upvote: false })
+      end
+    end
+
+    describe "#upvotes" do
+      it "returns the correct number of upvotes" do
+        expect(@user.upvotes.count).to eq @rand_num
+      end
+
+      it "returns a collection of upvotes" do
+        upvotes = @user.upvotes
+
+        expect(upvotes).to be_instance_of Array
+        expect(
+          upvotes.all? { |vote| vote.upvote? }
+          ).to be true
+      end
+    end
+
+    describe "#downvotes" do
+      it "returns a collection of downvotes" do
+        downvotes = @user.downvotes
+
+        expect(downvotes).to be_instance_of Array
+        expect(
+          downvotes.all? { |vote| vote.downvote? }
+          ).to be true
+      end
+
+      it "returns the correct number of downvotes" do
+        expect(@user.downvotes.count).to eq @rand_num
+      end
+    end
+  end
 end
